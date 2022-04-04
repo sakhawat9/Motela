a9c9047d0e458db8ad6ffd89e134c4fcd587fdcc
sakhawat9 <sakhawathossain7969@gmail.com>
2022-04-04 16:07:25 +0600

    [ADD] project setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
+import "./assets/styles/main.scss"
 
 function App() {
   return (
-    <div className="App">
+    <div className="hero">
       <h3>Hello world</h3>
     </div>
   );
