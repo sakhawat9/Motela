ce57b043787cbb4cbc60576ba190dc2202f8825d
sakhawat9 <sakhawathossain7969@gmail.com>
2022-04-06 00:40:56 +0600

    [ADD] serves section

--- src/components/Home/Services.js
+++ src/components/Home/Services.js
@@ -1,45 +1,280 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
+import { AiOutlineCheck } from "react-icons/ai";
 
 const Services = () => {
   return (
-    <div className="service">
-      <div className="container">
-        <div className="service__wrapper">
-          <div className="service__wrapper__first-item">
-            <div></div>
-          </div>
-          <div className="service__wrapper__content">
-            <h5 className="service__wrapper__content__subtitle">
-              FACILITIES &amp; MORE
-            </h5>
+    <>
+      <div className="service">
+        <div className="container">
+          <div className="service__wrapper">
+            <div className="service__wrapper__first-item">
+              <div></div>
+            </div>
+            <div className="service__wrapper__content">
+              <h5 className="service__wrapper__content__subtitle">
+                FACILITIES &amp; MORE
+              </h5>
 
-            <h1 className="service__wrapper__content__title">All Services</h1>
+              <h1 className="service__wrapper__content__title">All Services</h1>
 
-            <p className="service__wrapper__content__description">
-              Quisque vitae posuere libero. Phasellus feugiat erat sit amet dui
-              condimentum imperdiet. Ut at libero nec ligula fringilla dictum
-              sit amet id dui. Integer gravida dolor elit, sit amet vestibulum
-              mi elementum eget.
-            </p>
+              <p className="service__wrapper__content__description">
+                Quisque vitae posuere libero. Phasellus feugiat erat sit amet
+                dui condimentum imperdiet. Ut at libero nec ligula fringilla
+                dictum sit amet id dui. Integer gravida dolor elit, sit amet
+                vestibulum mi elementum eget.
+              </p>
 
-            <div className="service__wrapper__content__btn">
-              <a href="#" className="bg-primary" role="button">
-                REQUEST NOW
-              </a>
-              <div className="">
-                <a href="#" className="bg-white text-gray-900" role="button">
-                  CONTACT US
+              <div className="service__wrapper__content__btn">
+                <a href="#" className="bg-primary" role="button">
+                  REQUEST NOW
                 </a>
+                <div className="">
+                  <a href="#" className="bg-white text-gray-900" role="button">
+                    CONTACT US
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="service__wrapper__last-item">
-            <div></div>
+            <div className="service__wrapper__last-item">
+              <div></div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className="">
+        <div className="container">
+          <div className="grid grid-cols-12 gap-6">
+            <div className="col-span-12 md:col-span-4">
+              <div className="">
+                <img
+                  width="741"
+                  height="532"
+                  src="http://www.nicdarkthemes.com/themes/hotel-inn/wp/demo/inn-suites/wp-content/uploads/sites/2/2021/12/image-4.jpg"
+                  className=""
+                  alt=""
+                  loading="lazy"
+                />
+              </div>
+              <div className="">
+                <h5 className="">ROOM CLEANING</h5>
+              </div>
+              <div className="">
+                <h1 className="">
+                  150 <span style={{ fontSize: "30px" }}>$ </span>
+                  <span style={{ fontSize: "14px" }}> / month</span>
+                </h1>
+              </div>
+              <div className="">
+                <ul className="">
+                  <li className="">
+                    <span className="">
+                      <AiOutlineCheck />
+                    </span>
+                    <span className="">Suspendisse nec tincidunt</span>
+                  </li>
+                  <li className="">
+                    <span className="">
+                      <AiOutlineCheck />
+                    </span>
+                    <span className="">Vestibulum sit amet nibh</span>
+                  </li>
+                  <li className="">
+                    <span className="">
+                      <AiOutlineCheck />
+                    </span>
+                    <span className="">Proin id eros dapibus, dict</span>
+                  </li>
+                </ul>
+              </div>
+              <div className="">
+                <a href="#" className="" role="button">
+                  <span className="">
+                    <span className="">MORE INFO</span>
+                  </span>
+                </a>
+              </div>
+            </div>
+            <div className="col-span-12 md:col-span-4">
+              <div className=""></div>
+              <div className="">
+                <img
+                  width="741"
+                  height="532"
+                  src="http://www.nicdarkthemes.com/themes/hotel-inn/wp/demo/inn-suites/wp-content/uploads/sites/2/2021/12/image-3.jpg"
+                  className=""
+                  alt=""
+                  loading="lazy"
+                />
+              </div>
+              <div className="">
+                <h5 className="">FUL LAUNDRY</h5>{" "}
+              </div>
+              <div className="">
+                <h1 className="">
+                  180 <span style={{ fontSize: "30px" }}>$ </span>
+                  <span style={{ fontSize: "14px" }}> / month</span>
+                </h1>
+              </div>
+              <div className="">
+                <ul className="">
+                  <li className="">
+                    <span className="">
+                      <AiOutlineCheck />
+                    </span>
+                    <span className="">Suspendisse nec tincidunt</span>
+                  </li>
+                  <li className="">
+                    <span className="">
+                      <AiOutlineCheck />
+                    </span>
+                    <span className="">Vestibulum sit amet nibh</span>
+                  </li>
+                  <li className="">
+                    <span className="">
+                      <AiOutlineCheck />
+                    </span>
+                    <span className="">Proin id eros dapibus, dict</span>
+                  </li>
+                </ul>
+              </div>
+              <div className="">
+                <a href="#" className="" role="button">
+                  <span className="">
+                    <span className="">REQUEST</span>
+                  </span>
+                </a>
+              </div>
+            </div>
+            <div className="col-span-12 md:col-span-4">
+              <div className="">
+                <img
+                  width="741"
+                  height="532"
+                  src="http://www.nicdarkthemes.com/themes/hotel-inn/wp/demo/inn-suites/wp-content/uploads/sites/2/2021/12/image-2.jpg"
+                  className=""
+                  alt=""
+                  loading="lazy"
+                />
+              </div>
+              <div className="">
+                <h5 className="">MEALS INCLUDED</h5>{" "}
+              </div>
+              <div className="">
+                <h1 className="">
+                  165 <span style={{ fontSize: "30px" }}>$ </span>{" "}
+                  <span style={{ fontSize: "14px" }}> / month</span>
+                </h1>
+              </div>
+              <div className="">
+                <ul className="">
+                  <li className="">
+                    <span className="">
+                      <AiOutlineCheck />
+                    </span>
+                    <span className="">Suspendisse nec tincidunt</span>
+                  </li>
+                  <li className="">
+                    <span className="">
+                      <AiOutlineCheck />
+                    </span>
+                    <span className="">Vestibulum sit amet nibh</span>
+                  </li>
+                  <li className="">
+                    <span className="">
+                      <AiOutlineCheck />
+                    </span>
+                    <span className="">Proin id eros dapibus, dict</span>
+                  </li>
+                </ul>
+              </div>
+              <div className="">
+                <div className="">
+                  <a href="#" className="" role="button">
+                    <span className="">
+                      <span className="">SEE MENU</span>
+                    </span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="grid grid-cols-12 gap-4">
+            <div className="col-span-12 md:col-span-4">
+              <div className="">
+                <h5 className="">01</h5>{" "}
+              </div>
+
+              <div className="">
+                <p className="">SPA &amp; MASSAGES</p>
+              </div>
+              <div className="">
+                <p className="">
+                  Etiam diam arcu, semper at interdum id, ultricies quis ipsum.
+                  Duis laoreet ante feugiat,
+                </p>{" "}
+              </div>
+
+              <div className="">
+                <h5 className="">02</h5>{" "}
+              </div>
+
+              <div className="">
+                <p className="">ROOM SERVICE</p>
+              </div>
+
+              <div className="">
+                <p className="">
+                  Etiam diam arcu, semper at interdum id, ultricies quis ipsum.
+                  Duis laoreet ante feugiat,
+                </p>{" "}
+              </div>
+            </div>
+
+            <div className="col-span-12 md:col-span-4">
+              <div className="">
+                <h5 className="">03</h5>{" "}
+              </div>
+
+              <div className="">
+                <p className="">IRONING &amp; LAUNDRY</p>{" "}
+              </div>
+              <div className="">
+                <p className="">
+                  Etiam diam arcu, semper at interdum id, ultricies quis ipsum.
+                  Duis laoreet ante feugiat,
+                </p>
+              </div>
+
+              <div className="">
+                <h5 className="">04</h5>{" "}
+              </div>
+              <div className="">
+                <p className="">CLEANING ROOM</p>
+              </div>
+
+              <div className="">
+                <p className="">
+                  Etiam diam arcu, semper at interdum id, ultricies quis ipsum.
+                  Duis laoreet ante feugiat,{" "}
+                </p>
+              </div>
+            </div>
+            <div className="col-span-12 md:col-span-4">
+              <div className="">
+                <h5 className="">MEALS INCLUDED</h5>
+              </div>
+              <div className="">
+                <div className="">24</div>
+                <div className="">SEATS</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
