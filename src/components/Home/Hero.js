450e02cded3bcf9621fd98f036c0a276ff668061
monirul-fahad <immonirulfahad@gmail.com>
2022-04-06 00:58:46 +0600

    banner complete

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React from "react";
 
 const Hero = () => {
-    return (
-        <div className='hero'>
-            <h2>This is Hero Section</h2>
+  return (
+    <div className="hero">
+      <div className="hero__content">
+        <div className="hero__content__top">
+          <p>BOOK YOUR ROOM NOW !!</p>
+          <h2>Reserve</h2>
+          <h2>Your Holiday</h2>
         </div>
-    );
+        <div className='hero__content__bottom'>
+          <div className="hero__content__bottom__search">
+            <div>
+              <p>CHECK-IN</p>
+              <input type="text" value="5 Apr"></input>
+            </div>
+            <div>
+              <p>CHECK-OUT</p>
+              <input type="text" value="5 Apr"></input>
+            </div>
+            <div>
+              <p>GUESTS</p>
+              <input type="number" value="2"></input>
+            </div>
+            <div className='mt-8'>
+             
+              <input type="submit" value="CHECK AVAILABILITY"></input>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
